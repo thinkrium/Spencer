e354e5d41f87923a33a039abdd6eba25ab384cc6
= <=>
2023-01-16 18:11:33 -0600

    Init

---
+++ build/CMakeFiles/Robot.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Robot.dir/main.cpp.o"
+  "CMakeFiles/Robot.dir/main.cpp.o.d"
+  "Robot"
+  "Robot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Robot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/thomedy/Thinkrium/Robotics/SourceCode/Spencer/Operating_System/Spencer
+# Build directory: /home/thomedy/Thinkrium/Robotics/SourceCode/Spencer/Operating_System/Spencer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("Libraries")
+subdirs("Systems")
